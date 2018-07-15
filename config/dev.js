d9fc2689fd8eccd86ec8373ffc7b22958d744310
Cody Brunner <rockchalkwushock@icloud.com>
2018-07-15 15:17:01 -0700

    updates and getting started.

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb://rockchalkwushock:123abc@ds237641.mlab.com:37641/blog_dev',
   cookieKey: '123123123'
-};
+}
