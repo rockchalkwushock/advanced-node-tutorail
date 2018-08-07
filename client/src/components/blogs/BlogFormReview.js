70418254b1c9a715b9c1d40c14e58b189829eb6e
Cody Brunner <rockchalkwushock@icloud.com>
2018-08-07 10:06:49 -0700

    adds upload routes

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -1,14 +1,15 @@
 // BlogFormReview shows users their form inputs for review
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
+import _ from 'lodash'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import formFields from './formFields'
+import { withRouter } from 'react-router-dom'
+import * as actions from '../../actions'
 
 class BlogFormReview extends Component {
+  state = { file: null }
   renderFields() {
-    const { formValues } = this.props;
+    const { formValues } = this.props
 
     return _.map(formFields, ({ name, label }) => {
       return (
@@ -16,12 +17,12 @@ class BlogFormReview extends Component {
           <label>{label}</label>
           <div>{formValues[name]}</div>
         </div>
-      );
-    });
+      )
+    })
   }
 
   renderButtons() {
-    const { onCancel } = this.props;
+    const { onCancel } = this.props
 
     return (
       <div>
@@ -36,15 +37,19 @@ class BlogFormReview extends Component {
           <i className="material-icons right">email</i>
         </button>
       </div>
-    );
+    )
   }
 
   onSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    const { submitBlog, history, formValues } = this.props;
+    const { submitBlog, history, formValues } = this.props
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history)
+  }
+
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] })
   }
 
   render() {
@@ -53,14 +58,24 @@ class BlogFormReview extends Component {
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
 
+        <h5>Add an image</h5>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={this.onFileChange.bind(this)}
+        />
+
         {this.renderButtons()}
       </form>
-    );
+    )
   }
 }
 
 function mapStateToProps(state) {
-  return { formValues: state.form.blogForm.values };
+  return { formValues: state.form.blogForm.values }
 }
 
-export default connect(mapStateToProps, actions)(withRouter(BlogFormReview));
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(BlogFormReview))
---
+++ routes/uploadRoutes.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk')
+const {
+  v1
+} = require('uuid')
+
+const keys = require('../config/keys')
+const requireLogin = require('../middlewares/requireLogin')
+
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey
+})
+
+module.exports = app => {
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${v1()}.jpeg`
+    s3.getSignedUrl(
+      'putObject', {
+        Bucket: 'codys-blog-bucket',
+        ContentType: 'jpeg',
+        Key: key
+      },
+      (err, url) => res.send({
+        key,
+        url
+      })
+    )
+  })
+}
